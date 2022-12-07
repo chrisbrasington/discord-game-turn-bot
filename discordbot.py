4d31a242dc1f26de12b116bd459dc8e975727e10
Christopher Brasington <1186896+chrisbrasington@users.noreply.github.com>
2022-12-07 00:19:09 -0700

    active

--- discordbot.py
+++ discordbot.py
@@ -297,9 +297,9 @@ async def config(ctx):
         await ctx.channel.send(f"Listening on {game_channel}")
     
     if game_active:
-        await ctx.channel.send("Game is not active.")
-    else:
         await ctx.channel.send("Game is active.")
+    else:
+        await ctx.channel.send("Game is not active.")
     await print_simple(ctx)
 
 # command test
