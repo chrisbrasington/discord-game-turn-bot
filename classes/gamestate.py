8a4f08ddbfc5e44fa3533e2bc9fb7cda6a1a7299
Christopher Brasington <1186896+chrisbrasington@users.noreply.github.com>
2022-12-07 13:54:56 -0700

    refactor

---
+++ classes/gamestate.py
@@ -0,0 +1,289 @@
+import json, os, random
+
+class GameState:
+    game_state_file = 'gamestate.json'
+    player_file = 'players.json'
+    test_file = 'test.json'
+    channel = None
+    SECONDS_PER_HOUR = 3600
+
+    def __init__(self):
+        self.active = False
+
+        self.names = []
+        self.players = []
+        self.mapping = {}
+        self.alarm_hours = 2
+        self.is_test = False
+        self.index = 0
+        self.channel = '🤖bot-commands'
+
+        # self.ReadPlayers()
+        self.is_test = True
+        self.ReadTestPlayers()
+
+    def ReadPlayers(self):
+        self.names = []
+        self.players = []
+        print(f'Reading Players File {self.player_file}')
+        self.test = False
+        # read from file
+        if os.path.exists(self.player_file):
+        # Open the file in read mode
+            with open(self.player_file, 'r') as f:
+                # Read the JSON string from the file
+                json_string = f.read()
+
+            print(json_string)
+
+            # Convert the JSON string to a list of strings
+            self.names = json.loads(json_string)
+            self.players = self.names.copy()
+
+    def ReadTestPlayers(self):
+        self.names = []
+        self.players = []
+        print(f'Reading Test Players File: {self.test_file}')
+        self.test = True
+        # read from file
+        if os.path.exists(self.test_file):
+        # Open the file in read mode
+            with open(self.test_file, 'r') as f:
+                # Read the JSON string from the file
+                json_string = f.read()
+
+            print(json_string)
+
+            # Convert the JSON string to a list of strings
+            self.names = json.loads(json_string)
+            self.players = self.names.copy()
+    
+    async def TestMode(self, is_test: bool, bot):
+        self.names = []
+        self.players = []
+        self.is_test = is_test
+        print(f'TEST MODE: {is_test}')
+        if self.is_test:
+            self.ReadTestPlayers()
+        else:
+            self.ReadPlayers()
+
+        for name in self.names:
+            await self.ReadUser(bot, name)
+
+    async def DisplayConfig(self, ctx, bot):
+
+        print(await self.Serialize())
+
+        if self.channel is None:
+            output = 'Not listening to any channel'
+        else:
+            output = f'Listening on {self.channel}'
+        if self.test:
+            output += '\nTEST MODE ON'
+        if self.active:
+            output += '\nGame is active'
+        else:
+            output += '\nGame is not active'
+        if self.alarm_hours != 0:
+            output += f'\nAlarm is set to  {self.alarm_hours}'
+        else:
+            output += '\nAlarm is disabled'
+
+        await ctx.channel.send(output)
+
+        if self.mapping == {}:
+            await ctx.channel.send('Reading usernames first time... one moment please...')
+            for name in self.names:
+                await self.ReadUser(bot, name)
+
+        await ctx.channel.send(await self.PrintSimple())
+
+    async def PrintSimple(self):
+        output_list = []
+        for name in self.names:
+            user = self.mapping[name]
+            if name == user:
+                output_list.append(name)
+            else:
+                output_list.append(f'{user.name}#{user.discriminator}')
+        print(output_list)
+        return output_list
+
+    async def ReadAllUsers(self, bot):
+        for name in self.names:
+            await self.ReadUser(bot, name)
+
+    async def ReadUser(self, bot, name: str):
+        if '@' in name:
+            id = int(name.replace('<', '').replace('@', '').replace('>', ''))
+            user = await bot.fetch_user(id)
+            self.mapping[name] = user
+        else:
+            self.mapping[name] = name
+
+        print(f"read: {self.mapping[name]}")
+        return self.mapping[name]
+
+    async def Shuffle(self):
+        self.players = self.names.copy()
+        random.shuffle(self.players)
+        print(f'Shuffled: {self.players}')
+
+    async def Save(self):
+        if self.is_test:
+            print('Save, skipping in test mode')
+            return
+        print('Saving..')
+
+        with open(self.player_file, "w") as f:
+            # Write the JSON string to the file      
+            f.write(json.dumps(self.names))
+        
+        with open(self.game_state_file, "w") as f:
+            # Write the JSON string to the file      
+            f.write(json.dumps(self, cls=GameStateEncoder, indent=4, sort_keys=True))
+
+    async def Serialize(self):
+        return json.dumps(self, cls=GameStateEncoder, indent=4, sort_keys=True)
+
+    async def Add(self, bot, new_name: str):
+        print("add command:")
+
+        for single_name in new_name.split(","):
+            if(single_name != ''):
+                if(single_name in self.names):
+                    # await ctx.channel.send(f"{name} already exists")
+                    return False
+                else:
+                    single_name = single_name.strip()
+                    self.names.append(single_name)
+                    self.players.append(single_name)
+
+                    await self.ReadUser(bot, single_name)
+
+                    await self.Save()
+                    return True
+
+    async def Remove(self, name: str):
+        print(f'Removing {name}')
+        removed = False
+        if name in self.names:
+            self.names.remove(name)
+            removed = True
+        if name in self.players:
+            self.players.remove(name)
+            removed = True
+        
+        await self.Save()
+        return removed
+
+    async def Begin(self, ctx):
+        print('Starting game...')
+        self.players = self.names.copy()
+        random.shuffle(self.players)
+        print(f'shuffled: {self.players}')
+        self.active = True
+        await self.Display(ctx)
+
+    async def End(self, ctx):
+        self.active = False
+        print('Ending game...')
+        await ctx.channel.send(f"Game over! Congratulations {self.players[self.index]}! Start new with /begin")
+        self.index = 0
+
+    async def Display(self, ctx):
+        print('Printing game...')
+        # do not advance to new game here
+        if not self.active:
+            output = "Game is not active. Start with /begin"
+            print(output)
+            await ctx.channel.send(output)
+            return
+
+        alarm_text = 'Alarm is disabled'
+        # if self.alarm_hours > 0:
+        #     # set alarm reminder for active player
+        #     interval_text = format(self.alarm_hours/self.SECONDS_PER_HOUR, ".0f")
+        #     alarm_text = f"setting alarm to {interval_text} hour(s)"
+        #     print(alarm_text)
+        #     signal.signal(signal.SIGALRM, lambda signum, frame: 
+        #         # await alarm(ctx)
+        #         asyncio.create_task(message_alarm(ctx, signal))
+        #     )
+        #     signal.alarm(alarm_interval)
+
+        # no players to start
+        if(len(self.players) == 0):
+            await ctx.channel.send("Add players first with /add @\{name\} command")
+            return
+        
+        # account for player removal mid-game
+        if(self.index > len(self.players)-1):
+            self.index = len(self.players)-1
+
+        output = ""
+
+        # new game
+        if(self.index == 0):
+            output = "New Game begin! - "
+            output += f"{alarm_text}\n\n"
+
+        # current turn
+        output += f"{self.players[self.index]} it's your turn!\n\n"
+        # output += 'New player turn\n\n'
+
+        # all players
+        i = 0
+        for name in self.players:
+            if i == self.index:
+                output += "--> "
+            else:
+                output += "    "
+            i += 1
+
+            if '@' in name:
+                # id = int(name.replace("<", "").replace("@", "").replace(">", ""))
+                # user = await bot.fetch_user(id)
+                # m = ctx.guild.get_member(id)
+
+                user = self.mapping[name]
+
+                output += f'{user.name}\n'
+
+            else: 
+                output += f"{name}\n"
+
+        print(output)
+
+        await ctx.channel.send(output)
+
+    async def Next(self, ctx):
+        if(self.index != len(self.players)-1):
+            self.index += 1
+            await self.Display(ctx)
+        else:
+            if(self.active):
+                await self.End(ctx)
+            else:
+                await self.Begin(ctx) 
+
+class GameStateEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, GameState):
+
+            # Here, you would define the encoding for your GameState object.
+            # You can return the encoded object as a dictionary, or as a JSON
+            # string, depending on your needs.
+            return {
+                'names': obj.names, 
+                'players': obj.players, 
+            # 'mapping': obj.mapping,   # NOT SERIALIZABLE!!    
+                'alarm_hours': obj.alarm_hours,
+                'channel': obj.channel,
+                'test': obj.test,
+                'index': obj.index
+            }
+        # This is important: call the superclass method to raise an exception
+        # for unsupported types
+        return super().default(obj)
--- discordbot.py
+++ discordbot.py
@@ -3,22 +3,10 @@ import asyncio, discord, json, os, random, re, signal
 from discord.ext import commands
 from datetime import datetime, time
 import time as regular_time
+from classes.gamestate import GameState, GameStateEncoder
 
-# record all players to file
-player_file = "players.json"
-channel_file = "channel.txt"
-name_list = None
-game_channel = None
-test = False
-name_mapping = {}
-
-# track game state
-index = 0
-
-# alarm interval default
-alarm_interval = 3600*2
-
-game_active = False
+# game state
+state = None
 
 # create bot with / commands
 bot = commands.Bot(
@@ -28,171 +16,80 @@ bot = commands.Bot(
 
 # initialize players file read
 async def init():
-    global game_active
-    global game_channel
-    global name_list
-    global index
-    global name_mapping
-    index = 0
-    game_active = False
-    name_mapping = {}
+    global bot, state
+    state = GameState()
 
-    # read from file
-    if os.path.exists(player_file):
-        # Open the file in read mode
-        with open(player_file, "r") as f:
-            # Read the JSON string from the file
-            json_string = f.read()
-
-        # Convert the JSON string to a list of strings
-        name_list = json.loads(json_string)
-
-    if os.path.exists(channel_file):
-        # Open a file for reading 
-        with open(channel_file, 'r') as file:
-            # Read the contents of the file
-            game_channel = file.read()
-
-    if game_channel is None:
-        print("Not listening on any channel")
+    if state.channel is None:
+        print("Not is_listening on any channel")
     else:
-        print(f"Listening on {game_channel}")
-
-    if name_list is None:
-        name_list = []
+        print(f"is_listening on {state.channel}")
 
     # shuffle game list (silent begin)
-    global game_list
-    game_list = name_list.copy()
-    random.shuffle(game_list)
-    print(game_list)
+    await state.Shuffle()
     print("Silent Ready")
 
-# save players to file
-async def save():
-    global test
-    if test:
-        return
+    for name in state.names:
+        await state.ReadUser(bot, name)
 
-    print("Saving..")
-    # Open the file in write mode
-    with open(player_file, "w") as f:
-        # Write the JSON string to the file      
-        f.write(json.dumps(name_list))
+    print(await state.Serialize())
 
 # command hello
 @bot.command(description="Display simple hello")
 async def hello(ctx):
     await ctx.send("Hello, world!")
 
-def listening(ctx):
-    global game_channel
-    # print("listening check")
-    # print(str(ctx.channel) == game_channel)
-    return str(ctx.channel) == game_channel
+def is_listening(ctx):
+    global state
+    return str(ctx.channel) == state.channel
+
+# command listen - sets game channel
+@bot.command()
+async def listen(ctx):
+    global state
+    state.channel = str(ctx.channel)
+    print(f"/listen {state.channel}")
+    await ctx.channel.send(f"Now is_listening on {ctx.channel}")
+    await state.Save()
 
 # command add player
 @bot.command(description="Adds player to game")
 async def add(ctx, names: str):
-    if(not listening(ctx)):
+    global bot, state 
+    if(not is_listening(ctx)):
         return
 
-    print("add command:")
-
-    for name in names.split(","):
-        if(name != ''):
-            if(name in name_list):
-                await ctx.channel.send(f"{name} already exists")
-            else:
-                name = name.strip()
-                name_list.append(name)
-                game_list.append(name)
-
-                read_username_from_server(name)
-    # name_list = name_list.sort()
-
-    await save()
-    await print_simple(ctx)
-    await print_game(ctx)
-
-# command clear 
-# @bot.command()
-# async def clear(ctx):
-#     if(not listening(ctx)):
-#         return
-    # global index
-    # index = 0
-    # name_list.clear()
-    # game_list.clear()
-    # print(name_list)
-    # os.remove(player_file)
-    # await ctx.channel.send("All players deleted")
-    # await ctx.channel.send("No")
+    if await state.Add(bot, names):
+        await ctx.channel.send("Added Player")
+        await state.DisplayConfig(ctx, bot)
+    else:
+        await ctx.channel.send(f"{name} already exists")
 
 # command removes a player from the game
 @bot.command()
 async def remove(ctx, name: str):
-    if(not listening(ctx)):
+    global state
+    if(not is_listening(ctx)):
         return
-    
-    global game_active
 
-    global index
-    found = False
-    if name in name_list:
-        found = True
-        name_list.remove(name)
-    if name in game_list:
-        game_list.remove(name)
-
-    if found:
-        await save()
+    if await state.Remove(name):
         await ctx.channel.send(f"Removed {name}")
-        await print_simple(ctx)
-
-        if game_active:
-            await print_game(ctx)
-
+        await state.DisplayConfig(ctx, bot)
     else:
         await ctx.channel.send(f"{name} not found")
 
 # command being
 @bot.command(aliases=["go", "start", "random", "randomize"])
 async def begin(ctx):
-    global name_mapping
-    if name_mapping == {}:
-        await ctx.channel.send("Getting game ready...")
-        _ = await get_simple()
-
-    if(not listening(ctx)):
-        return
-    global index 
-    index = 0
-    global game_active
-    game_active = True
-
-    global game_list 
-    game_list = name_list.copy()
-    random.shuffle(game_list)
-
-    await(print_game(ctx))
+    global state
+    await state.Begin(ctx)
 
 # command next/skip
 @bot.command(aliases=["skip"])
 async def next(ctx):
-    if(not listening(ctx)):
+    if(not is_listening(ctx)):
         return
-    global game_active
-    global index
-    print(index)
-    if(index != len(game_list)-1):
-        index += 1
-        await print_game(ctx)
-    else:
-        if(game_active):
-            await end_game(ctx)
-        else:
-            await begin(ctx) 
+    global state
+    await state.Next(ctx)
 
 # message alarm reminder for active player
 # do not message at night-time
@@ -229,7 +126,7 @@ def can_message_during_daytime():
 # command end
 @bot.command()
 async def end(ctx):
-    if(not listening(ctx)):
+    if(not is_listening(ctx)):
         return
     await end_game(ctx)
 
@@ -257,251 +154,91 @@ async def alarm(ctx, new_alarm: str):
 # command print, status
 @bot.command(name="print", aliases=["status", "who"])
 async def print_game(ctx):
-    if(not listening(ctx)):
-        return
-    global game_active
-    global alarm_interval
-    global name_mapping
-
-    SECONDS_PER_HOUR = 3600
-
-    alarm_text = "no alarm"
-
-    # do not advance to new game here
-    if not game_active:
-        output = "Game is not active. Start with /begin"
-        print(output)
-        await ctx.channel.send(output)
-        return
-
-    if alarm_interval > 0:
-        # set alarm reminder for active player
-        interval_text = format(alarm_interval/SECONDS_PER_HOUR, ".0f")
-        alarm_text = f"setting alarm to {interval_text} hour(s)"
-        print(alarm_text)
-        signal.signal(signal.SIGALRM, lambda signum, frame: 
-            # await alarm(ctx)
-            asyncio.create_task(message_alarm(ctx, signal))
-        )
-        signal.alarm(alarm_interval)
-
-    global index
-
-    # no players to start
-    if(len(game_list) == 0):
-        await ctx.channel.send("Add players first with /add @\{name\} command")
-        return
-    
-    # account for player removal mid-game
-    if(index > len(game_list)-1):
-        index = len(game_list)-1
-
-    output = ""
-
-    # new game
-    if(index == 0):
-        output = "New Game begin! - "
-        output += f"{alarm_text}\n\n"
-
-    # current turn
-    output += f"{game_list[index]} it's your turn!\n\n"
-
-    # all players
-    i = 0
-    for name in game_list:
-        if i == index:
-            output += "--> "
-        else:
-            output += "    "
-        i += 1
-
-        if '@' in name:
-            # id = int(name.replace("<", "").replace("@", "").replace(">", ""))
-            # user = await bot.fetch_user(id)
-            # m = ctx.guild.get_member(id)
-
-            user = name_mapping[name]
-
-            output += f'{user.name}\n'
-
-        else: 
-            output += f"{name}\n"
-
-    print(output)
-
-    await ctx.channel.send(output)
-
-# print only what exists in saved name list (not game list)
-async def print_simple(ctx):
-    global name_mapping
-    if(name_mapping == {}):
-        await ctx.channel.send("Reading usernames first time... one moment please...")
-
-    await ctx.channel.send(await get_simple())
-
-async def read_username_from_server(name: str):
-    global name_mapping
-    if '@' in name:
-        id = int(name.replace("<", "").replace("@", "").replace(">", ""))
-        user = await bot.fetch_user(id)
-        name_mapping[name] = user
-    else:
-        name_mapping[name] = name
-
-    return name_mapping[name]
-
-async def get_simple():
-    global name_mapping
-    output_list = []
-
-    if(name_mapping == {}):
-        print('reading usernames from server...')
-        for name in name_list:
-            await read_username_from_server(name)
-
-    for name in name_list:
-        user = name_mapping[name]
-        if name == user:
-            output_list.append(name)
-        else:
-            output_list.append(f'{user.name}#{user.discriminator}')
-
-    print(output_list)
-    return str(output_list)
-
-
+    global state
+    await state.Display(ctx)
+ 
 # end game without starting again
 async def end_game(ctx):
-    global index
-    global game_active
-    game_active = False
-    prior_index = index
-    index = len(game_list)-1
-    print("Game inactive")
-    await ctx.channel.send(f"Game over! Congratulations {game_list[prior_index]}! Start new with /begin")
+    global state
+    await state.End(ctx)
 
 # command test
 @bot.command()
 async def config(ctx):
-    # always listen
-    if game_channel is None:
-        await ctx.channel.send("Not listening on any channel")
-    else:    
-        await ctx.channel.send(f"Listening on {game_channel}")
-    
-    if alarm_interval == 0:
-        await ctx.channel.send("Alarm is disabled")
-    else:
-        plural = ""
-        if alarm_interval>3600 > 1:
-            plural = "s"
-        await ctx.channel.send(f"Alarm is set to {alarm_interval/3600} hour{plural}")
-
-    if game_active:
-        await ctx.channel.send("Game is active.")
-    else:
-        await ctx.channel.send("Game is not active.")
-    await print_simple(ctx)
-
-# command listen - sets game channel
-@bot.command()
-async def listen(ctx):
-    # always listen
-    global game_channel
-    game_channel = str(ctx.channel)
-
-    with open('channel.txt', 'w') as file:
-        # Write the string to the file
-        file.write(game_channel)
-
-    print(f"/listen {game_channel}")
-    await ctx.channel.send(f"Now listening on {ctx.channel}")
+    global state
+    await state.DisplayConfig(ctx, bot)
 
 # command test - sets players to test players
 @bot.command(name="gametest", aliases=["testmode", "goblinmode"])
 async def gametest(ctx):
-    global game_active
-    global name_list
-    global test    
-    global name_mapping
-    name_mapping = {}
-    game_active = False
-    test = True
-    await ctx.channel.send("Switching to test mode")
-
-     # read from file
-    if os.path.exists("test.json"):
-        # Open the file in read mode
-        with open("test.json", "r") as f:
-            # Read the JSON string from the file
-            json_string = f.read()
-
-        # Convert the JSON string to a list of strings
-        name_list = json.loads(json_string)
-
-    await config(ctx)
+    global state
+    await state.TestMode(True, bot)
+    await state.ReadAllUsers(bot)
+    await state.DisplayConfig(ctx, bot)
 
 # command restart - can unload test to real players
 @bot.command()
 async def restart (ctx):
-    init()
+    global state
+    state.ReadPlayers()
+
     await ctx.channel.send("Restarted")
-    await config(ctx)
+    await state.ReadAllUsers(bot)
+    await state.DisplayConfig(ctx, bot)
 
 # bot on message to channel
 @bot.event
-async def on_message(message):
-    # always listen
-    global game_channel
+async def on_message(ctx):
+    global state
 
-    if message.author == bot.user:
+    if ctx.author == bot.user:
         return
 
-    if(message.channel.type == discord.ChannelType.private):
-        await message.channel.send(f"Why are you DM-ing me {message.author.mention}? ya weirdo.")
-        await message.channel.send("Play games with me in your discord channel, check out the readme at https://github.com/chrisbrasington/discord-game-turn-bot")
-        print(f"{message.author.mention} send a dm, replying and ignoring")
+    if(ctx.channel.type == discord.ChannelType.private):
+        await ctx.channel.send(f"Why are you DM-ing me {ctx.author.mention}? ya weirdo.")
+        await ctx.channel.send("Play games with me in your discord channel, check out the readme at https://github.com/chrisbrasington/discord-game-turn-bot")
+        print(f"{ctx.author.mention} send a dm, replying and ignoring")
         return
     
-    image_responding_channel = str(message.channel) == game_channel
+    image_responding_channel = str(ctx.channel) == state.channel
 
-    # Use a regular expression to remove any Discord ID from message.content.
-    message_text = re.sub(r"<@\d+>\s*", "", message.content)
+    # Use a regular expression to remove any Discord ID from ctx.content.
+    message_text = re.sub(r"<@\d+>\s*", "", ctx.content)
     
-    # print(f"{message.author.mention} sent {message_text}")
+    # print(f"{ctx.author.mention} sent {message_text}")
     # print(image_responding_channel)
     # await print_simple(message)
 
-    # message inteded for bot
-    if bot.user in message.mentions:
+    # message intended for bot
+    if bot.user in ctx.mentions:
         print("Message intended for bot")
 
     # bot was mentioned
-    if bot.user in message.mentions:
+    if bot.user in ctx.mentions:
         # respond to hello
         if ("hello" in message_text or "hi" in message_text):
-            # Construct the response message.
-            response = f"Hello {message.author.mention}! How are you doing?"
-            await message.channel.send(response)
+            # Construct the response ctx.
+            response = f"Hello {ctx.author.mention}! How are you doing?"
+            await ctx.channel.send(response)
         # not understood
         elif("right" in message_text):
-            await message.channel.send("Fuck yeah {message.author.mention}")
+            await ctx.channel.send("Fuck yeah {ctx.author.mention}")
         elif("why" in message_text or "what" in message_text):
-            await message.channel.send("Sorry.. go ask chat.openai")
+            await ctx.channel.send("Sorry.. go ask chat.openai")
         elif("config" in message_text):
-            await config(message)
+            await state.DisplayConfig(ctx, bot)
         else:
-            await message.channel.send(f"{message_text}, you too {message.author.mention}.")
+            await ctx.channel.send(f"{message_text}, you too {ctx.author.mention}.")
 
     # if active player responding
-    if len(game_list) > 0:
-        if(image_responding_channel and str(message.author.id) in game_list[index]):
+    if len(state.players) > 0:
+        if(image_responding_channel and str(ctx.author.id) in state.players[state.index]):
             # print("Active player is responding")
             containsImage = False
 
             # image detection
-            if message.attachments:
-                for attachment in message.attachments:
+            if ctx.attachments:
+                for attachment in ctx.attachments:
                     # if attachment.is_image:
                     if attachment.filename.endswith((".png", ".jpg", ".gif")):
                         print("Progressing game")
@@ -509,22 +246,18 @@ async def on_message(message):
 
                         # progress
                         if(index == len(game_list)-1):
-                            await end_game(message)
+                            await state.End(ctx)
                             break
                         else:
-                            await next(message)
+                            await state.Next(ctx)
                             break
             # do not progress
             if not containsImage:
                 print("Active player is chatting")
 
-    if message.content.startswith('/'):
-
-        # user = message.author.mention
-
-        print(f"{message.author} sent {message_text}")
+    if ctx.content.startswith('/'):
+        print(f"{ctx.author} sent {message_text}")
         await bot.process_commands(message)
-
 
 # Open the file in read-only mode.
 with open("bot_token.txt", "r") as f:
