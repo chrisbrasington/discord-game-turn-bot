10c994054c2592af51edeecae936c22cbc71fd3a
Christopher Brasington <1186896+chrisbrasington@users.noreply.github.com>
2022-12-09 13:20:14 -0700

    startup

--- discordbot.py
+++ discordbot.py
@@ -46,7 +46,7 @@ async def on_ready():
     await state.ReadAllUsers(bot)
 
     if(state.active):
-        print(f'Ready. Current player: {state.players[state.index]}')
+        print(f'Ready. Current player: {state.mapping[state.index]}')
         await state.Status_Listening(bot, state.players[state.index])
     else:
         print('Ready, no game active')
